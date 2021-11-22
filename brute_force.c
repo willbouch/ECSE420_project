bfbe94cf68c23fab500c72e586024aea8004678f
willbouch <william.bouchard3@mail.mcgill.ca>
2021-11-22 16:15:32 -0500

    sequential done

--- brute_force.c
+++ brute_force.c
@@ -2,30 +2,98 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 
 #include "file_parser.h"
 
-int brute_force(char *truePassword, char **passwordBank, int length)
-{
-    for (int i = 0; i < length; i++)
+ char CHARS_MODE_0[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+#define LENGTH_MODE_0 10
+
+ char CHARS_MODE_1[] = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+#define LENGTH_MODE_1 36
+
+ char CHARS_MODE_2[] = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+#define LENGTH_MODE_2 62
+
+ char CHARS_MODE_3[] =
     {
-        if (strcmp(truePassword, passwordBank[i]) == 0)
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '!', '?', '$', '#', '&', '*'};
+
+#define LENGTH_MODE_3 68
+
+char *generatePassword(int pwLength, int seed)
+{
+    int quotient = seed;
+    int remainder;
+    char *pw = (char *)malloc(pwLength * sizeof(char));
+    for (int i = pwLength - 1; i >= 0; i--)
+    {
+        remainder = quotient % LENGTH_MODE_3;
+        quotient = quotient / LENGTH_MODE_3;
+        char c = CHARS_MODE_3[remainder];
+        pw[i] = c;
+    }
+    pw[pwLength] = '\n';
+    return pw;
+}
+
+int bruteForce(char *truePassword, int pwLength, char* chars, int charsLength)
+{
+    long maxIndex = (long) pow(charsLength, pwLength);
+    for (int i = 0; i < maxIndex; i++)
+    {
+        char* testPassword = generatePassword(pwLength, i);
+        if (strcmp(truePassword, testPassword) == 0)
         {
             return 1;
         }
+        free(testPassword);
     }
     return 0;
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    char *passwordsFilePath = "passwords_4.txt";
-    char *permutationsFilePath = "all_chars_4.txt";
-    int nPasswords;
-    int nPermutations;
+    // Arguments
+    char *passwordsFilePath = argv[1];
+    int pwLength = atoi(argv[2]);
+    int mode = atoi(argv[3]);
 
+    // Set up the test password list
+    int nPasswords;
     char **passwords = parseFile(passwordsFilePath, &nPasswords);
-    char **permutations = parseFile(permutationsFilePath, &nPermutations);
+
+    char *chars;
+    int charsLength;
+    switch (mode)
+    {
+    case 0:
+        chars = CHARS_MODE_0;
+        charsLength = LENGTH_MODE_0;
+        break;
+    case 1:
+        chars = CHARS_MODE_1;
+        charsLength = LENGTH_MODE_1;
+        break;
+    case 2:
+        chars = CHARS_MODE_2;
+        charsLength = LENGTH_MODE_2;
+        break;
+    case 3:
+        chars = CHARS_MODE_3;
+        charsLength = LENGTH_MODE_3;
+        break;
+    }
 
     // TO REMOVE
     nPasswords = 500;
@@ -34,7 +102,7 @@ int main(int argc, char const *argv[])
     for (int i = 0; i < nPasswords; i++)
     {
         clock_t start = clock();
-        int res = brute_force(passwords[i], permutations, nPermutations);
+        int res = bruteForce(passwords[i], pwLength, chars, charsLength);
         clock_t end = clock();
         double timeSpent = (double)(end - start) / CLOCKS_PER_SEC;
         timeSum += timeSpent;
--- permutations_and_passwords.py
+++ permutations_and_passwords.py
@@ -1,18 +1,14 @@
 import numpy as np
 import string
 import random
-import itertools
 
 digits = [d for d in string.digits]
 lower_case_letters = [l for l in string.ascii_lowercase]
 upper_case_letters = [u for u in string.ascii_uppercase]
 symbols = ['!', '?', '$', '#', '&', '*']
 
-num_passwords = 10000
-max_characters_numeric = 6
-max_characters_lc_alphanum = 4
-max_characters_alphanum = 4
-max_characters_all = 4
+num_passwords = 1000
+max_character = 10
 
 # N represents the number of passwords
 # M represents the number of characters
@@ -22,71 +18,46 @@ def generate_passwords(N, M, char_bank):
     passwords.append(''.join([random.choice(char_bank) for _ in range(M)]))
   return np.array(passwords, dtype=object)
 
-# M represents the number of characters
-def generate_permutations(M, char_bank):
-  permutations = []
-  combinations_raw = list(itertools.product(char_bank, repeat = M))
-  for c in combinations_raw:
-    permutations.append(''.join(c)) # We store the hash since Numba doesn't support strings
-  return np.array(permutations, dtype=object)
-
 # Numeric Characters
 numeric_chars = digits
-numeric_passwords = generate_passwords(num_passwords, max_characters_numeric, numeric_chars)
+numeric_passwords = generate_passwords(num_passwords, max_character, numeric_chars)
 
 # Lowercase Alphanumeric Characters
 lc_alphanum_chars = lower_case_letters + digits
-lc_alphanum_passwords = generate_passwords(num_passwords, max_characters_lc_alphanum, lc_alphanum_chars)
+lc_alphanum_passwords = generate_passwords(num_passwords, max_character, lc_alphanum_chars)
 
 # Alphanumeric (lowercase & uppercase) Characters
 alphanum_chars = lower_case_letters + upper_case_letters + digits
-alphanum_passwords = generate_passwords(num_passwords, max_characters_alphanum, alphanum_chars)
+alphanum_passwords = generate_passwords(num_passwords, max_character, alphanum_chars)
 
 # Alphanumeric Characters and Symbols
 all_chars = lower_case_letters + upper_case_letters + digits + symbols
-all_passwords = generate_passwords(num_passwords, max_characters_all, all_chars)
+all_passwords = generate_passwords(num_passwords, max_character, all_chars)
 
-# We go with a toggle system to avoid overloading the RAM
-flag = 3
-
-# Numeric Characters
-if flag == 0:
-  permutations = generate_permutations(max_characters_numeric, numeric_chars)
-  passwords = numeric_passwords
-  max_char = max_characters_numeric
-  perm_type = 'numeric_chars'
-
-# Lowercase Alphanumeric Characters
-if flag == 1:
-  permutations = generate_permutations(max_characters_lc_alphanum, lc_alphanum_chars)
-  passwords = lc_alphanum_passwords
-  max_char = max_characters_lc_alphanum
-  perm_type = 'lc_alphanum_chars'
-
-# Alphanumeric (lowercase & uppercase) Characters
-if flag == 2:
-  permutations = generate_permutations(max_characters_alphanum, alphanum_chars)
-  passwords = alphanum_passwords
-  max_char = max_characters_alphanum
-  perm_type = 'alphanum_chars'
-
-# Alphanumeric Characters and Symbols
-if flag == 3:
-  permutations = generate_permutations(max_characters_all, all_chars)
-  passwords = all_passwords
-  max_char = max_characters_all
-  perm_type = 'all_chars'
-
-file_name = perm_type + '_' + str(max_char) + '.txt'
+file_name = './passwords/digits_passwords_' + str(max_character) + '.txt'
 f = open(file_name, 'w')
-f.write(str(len(permutations)) + '\n')
-for p in permutations:
+f.write(str(len(numeric_passwords)) + '\n')
+for p in numeric_passwords:
   f.write(p + '\n')
 f.close()
 
-file_name = 'passwords_' + str(max_char) + '.txt'
+file_name = './passwords/lc_alphanum_passwords_' + str(max_character) + '.txt'
 f = open(file_name, 'w')
-f.write(str(len(passwords)) + '\n')
-for p in passwords:
+f.write(str(len(lc_alphanum_passwords)) + '\n')
+for p in lc_alphanum_passwords:
+  f.write(p + '\n')
+f.close()
+
+file_name = './passwords/alphanum_passwords_' + str(max_character) + '.txt'
+f = open(file_name, 'w')
+f.write(str(len(alphanum_passwords)) + '\n')
+for p in alphanum_passwords:
+  f.write(p + '\n')
+f.close()
+
+file_name = './passwords/all_passwords_' + str(max_character) + '.txt'
+f = open(file_name, 'w')
+f.write(str(len(all_passwords)) + '\n')
+for p in all_passwords:
   f.write(p + '\n')
 f.close()
